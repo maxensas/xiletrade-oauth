2b7417fea95272422456dde3b4f4251cfd8219ce
maxensas <xiletrade@gmail.com>
2025-10-04 12:29:32 +0200

    Adding auth0 authentication

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,41 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using XiletradeAuth;
+using XiletradeAuth.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<WeatherService>();
+
+var navManager = builder.Services.BuildServiceProvider().GetRequiredService<NavigationManager>();
+var baseUri = new Uri(navManager.BaseUri);
+string currentHost = baseUri.Host;
+//var environment = hostname.Contains("localhost") ? "Development" : "Production";
+var auth0Section = currentHost.Contains("localhost") 
+    ? builder.Configuration.GetSection("Auth0localhost") : builder.Configuration.GetSection("Auth0");
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    auth0Section.Bind(options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code"; // PKCE flow
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+
+    options.AuthenticationPaths.LogOutCallbackPath = "/authentication/logout-callback";
+    options.AuthenticationPaths.LogOutFailedPath = "/authentication/logout-failed";
+
+    // override the end session endpoint
+    /* .NET 10
+    options.ProviderOptions.MetadataSeed = new Dictionary<string, object>
+    {
+        ["end_session_endpoint"] = "https://xiletrade.eu.auth0.com/v2/logout"
+    };
+    */
+});
 
 await builder.Build().RunAsync();
---
+++ src/Services/WeatherService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using XiletradeAuth.Models;
+
+namespace XiletradeAuth.Services;
+
+public class WeatherService
+{
+    private readonly HttpClient _http;
+
+    public WeatherService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<IEnumerable<WeatherForecast>> GetForecastAsync()
+    {
+        return await _http?.GetFromJsonAsync<IEnumerable<WeatherForecast>>("sample-data/weather.json");
+    }
+}
